58699f7176483f71c79a346ce99266847f05c647
Carlos Ceron <carlos.ceron.1986@gmail.com>
2021-03-29 18:35:08 -0500

    Modificaciones en Zappy.

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from posts.models import Post
 from posts.serializers import PostSerializer
 
@@ -7,4 +7,8 @@ from posts.serializers import PostSerializer
 class PostList(generics.ListAPIView):
 	queryset = Post.objects.all()
 	serializer_class = PostSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+	
+	def perform_create(self, serializer):
+		serializer.save(poster=self.request.user)
 
